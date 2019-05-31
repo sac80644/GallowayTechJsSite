e08933508b61d4c0961bd7a5e879124bc677246f
sac80644 <jason@gallowaytech.com>
2019-05-31 17:16:09 -0500

    ajax error handling

--- src/servers/ajax.js
+++ src/servers/ajax.js
@@ -8,25 +8,29 @@ function GetXhr(callback) {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function(){
         if (xhr.readyState === 4){
-            var response = xhr.responseText;
-            callback(response);
+            if(xhr.status === 200) {
+                var response = xhr.responseText;
+                callback(response);
+            } else {
+                callback("Request error: " + xhr.responseText + ' ' + xhr.status + ' ' + xhr.statusText);
+            }
         }
     };
 
-    xhr.open('GET', 'http://localhost:8080/data/mockdata.json');
+    xhr.open('GET', 'http://localhost:8080/data/dynamic.html');
     xhr.send();
 }
 
 function GetJQuery(callback) {
     $.ajax({
-        url : 'http://localhost:8080/data/mockdata.json',
+        url : 'http://localhost:8080/data/dynamic.html',
         type : 'GET',
         success : function(data) {              
-            callback(JSON.stringify(data));
+            callback(data);
         },
         error : function(request, error)
         {
-            callback("Request: " + error + ' ' + request.responseText + ' ' + request.status + ' ' + request.statusText);
+            callback("Request error: " + request.responseText + ' ' + request.status + ' ' + request.statusText);
         }
     });
 }
