0029507da0a233ac7d6f6a68582d6093a16a659c
sac80644 <jason@gallowaytech.com>
2019-06-04 18:36:03 -0500

    begin styling

---
+++ src/header/header.js
@@ -0,0 +1,9 @@
+import $ from 'jquery';
+import './header.css';
+import content from './header.html';
+
+export default content;
+
+$(document).ready(function() {
+
+});
--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import $ from 'jquery';
+import contentHeader from './header/header.js';
 import contentAbout from './about/about.js';
 import contentContact from './contact/contact.js';
-import contentWork from './work/work.js';
+import contentWork from './portfolio/portfolio.js';
 import { Get } from './servers/ajax.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './site.css';
 
 $(document).ready(function() {
+    $("#Header").replaceWith(contentHeader);
     $("#About").replaceWith(contentAbout);
     $("#Contact").replaceWith(contentContact);
-    $("#Work").replaceWith(contentWork);
+    $("#Portfolio").replaceWith(contentWork);
     Get(
         function(content) {
             $('#ajaxContent').replaceWith(content);
@@ -18,11 +20,14 @@ $(document).ready(function() {
 });
 
 if(module.hot) {
+    module.hot.accept('./header/header.js', function() {
+        console.log('accepting hmr from header');
+    })
     module.hot.accept('./about/about.js', function() {
         console.log('accepting hmr from about');
     })
-    module.hot.accept('./work/work.js', function() {
-        console.log('accepting hmr from work');
+    module.hot.accept('./portfolio/portfolio.js', function() {
+        console.log('accepting hmr from portfolio');
     })
     module.hot.accept('./contact/contact.js', function() {
         console.log('accepting hmr from contact');
