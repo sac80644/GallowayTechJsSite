96fa291b7addcf9b9241758804c3e877ebc8f8fe
sac80644 <jason@gallowaytech.com>
2019-06-22 00:50:39 -0500

    some boiler plate content

--- src/content/content.js
+++ src/content/content.js
@@ -1,9 +1,11 @@
 import $ from 'jquery';
 import './content.css';
 import patterns from './patterns.html';
+import development from './development.html';
 
 let content = {
     patterns,
+    development,
     hook: function() {
         //put a hook here...
     }
