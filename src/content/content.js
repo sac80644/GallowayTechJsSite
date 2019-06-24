ff57bd74c706f92106604747c6a9483e47d6dc39
sac80644 <jason@gallowaytech.com>
2019-06-24 11:06:38 -0500

    final latin boiler plates

--- src/content/content.js
+++ src/content/content.js
@@ -2,10 +2,12 @@ import $ from 'jquery';
 import './content.css';
 import patterns from './patterns.html';
 import development from './development.html';
+import recent from './recent.html';
 
 let content = {
     patterns,
     development,
+    recent,
     hook: function() {
         //put a hook here...
     }
