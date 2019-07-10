844b714ae0d21de472ab5e5893734d2eaf1136a9
sac80644 <jason@gallowaytech.com>
2019-07-10 11:28:15 -0500

    more content

--- src/content/content.js
+++ src/content/content.js
@@ -3,11 +3,13 @@ import './content.css';
 import patterns from './patterns.html';
 import development from './development.html';
 import recent from './recent.html';
+import professional from './professional.html';
 
 let content = {
     patterns,
     development,
     recent,
+    professional,
     hook: function() {
         //put a hook here...
     }
